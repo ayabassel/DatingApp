9f3db1ec6700221c3569fc947cded928fac44d06
Aya-Bassel <ayabassell@gmail.com>
2020-01-24 09:43:47 +0200

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly DataContext Context;
+
+        public ValuesController(DataContext context)
+        {
+           
+            Context = context;
+        }
+        // GET api/values   
+        [HttpGet]
+        public async Task<IActionResult> Getvalues()
+        {
+            var values = await Context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Getvalues(int id)
+        {
+            var values = await Context.Values.FirstOrDefaultAsync(x => x.id == id);
+            return Ok(values);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Model/Value.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.Model
+{
+    public class Value
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+}
